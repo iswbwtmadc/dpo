fefefce39c0dca540980704a1d60cd82727c675f
iswbwtmadc <71062785+iswbwtmadc@users.noreply.github.com>
2024-04-09 15:22:12 +0300

    Update dpo.py

--- dpo.py
+++ dpo.py
@@ -6,3 +6,8 @@ dataframe = pd.DataFrame(
     np.random.randn(10, 20),
     columns=('col %d' % i for i in range(20)))
 st.table(dataframe)
+
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
