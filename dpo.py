d723f51913ab58f0092ab7a4c3d9b8066c63ffaf
iswbwtmadc <71062785+iswbwtmadc@users.noreply.github.com>
2024-04-09 15:44:21 +0300

    Update dpo.py

--- dpo.py
+++ dpo.py
@@ -2,30 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-st.table(dataframe)
-
-st.text_input("Your name", key="name")
-
-# You can access the value at any point with:
-st.session_state.name
-
-
-with st.chat_message("user"):
-    st.write("Hello 👋")
-    
-with st.chat_message("assistant"):
-    st.write("Hello human")
-    st.bar_chart(np.random.randn(30, 3))
-
-import streamlit as st
-
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
-
 import random
 import time
 
